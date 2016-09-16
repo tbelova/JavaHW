d2f8ba9e58853f52123c716bb601452d3f69126b
tbelova <yukikomodo@gmail.com>
2016-09-16 23:54:14 +0300

    add comments

--- JavaHW1/src/com/company/HashTable.java
+++ JavaHW1/src/com/company/HashTable.java
@@ -1,10 +1,17 @@
 package com.company;
 
+/*
+
+HashTable -- класс, представляющий из себя хеш-таблицу с ключами и значениями типа String
+
+ */
+
 public class HashTable {
     private static final int cntOfLists = 239;
 
     private int sz = 0;
     private List[] arrayOfLists = new List[cntOfLists];
+
 
     public HashTable() {
         for (int i = 0; i < cntOfLists; i++) {
@@ -23,18 +30,35 @@ public class HashTable {
         return hash;
     }
 
+
+    /*
+     метод size() возвращает количество ключей в хеш-таблице
+     */
     public int size() {
         return sz;
     }
 
+
+    /*
+    метод contains() возвращает true, если данный ключ содержится в хеш-таблице, и false иначе
+     */
     public boolean contains(String key) {
         return !(arrayOfLists[Hash(key)].empty());
     }
 
+
+    /*
+     метод get() возвращает значение по ключу, или null, если такого значения нет
+     */
     public String get(String key) {
         return arrayOfLists[Hash(key)].get(key);
     }
 
+
+    /*
+    метод put(key, value) кладет в хеш-таблицу значение value по ключу key и возвращает то,
+    что было по этому ключу раньше, либо null, ели ничего не было
+     */
     public String put(String key, String value) {
         String prevValue = arrayOfLists[Hash(key)].insert(key, value);
         if (prevValue == null) {
@@ -43,6 +67,11 @@ public class HashTable {
         return prevValue;
     }
 
+
+    /*
+    метод remove(key) удаляет значение по заданному ключу из хеш-таблицы и возвращает удалённое значение,
+    либо null, если такого значения не было
+     */
     public String remove(String key) {
         String prevValue = arrayOfLists[Hash(key)].erase(key);
         if (prevValue != null) {
@@ -51,6 +80,10 @@ public class HashTable {
         return prevValue;
     }
 
+
+    /*
+    метод clear() очищает хеш-таблицу
+     */
     public void clear() {
         sz = 0;
         for (List ml: arrayOfLists) {
--- JavaHW1/src/com/company/List.java
+++ JavaHW1/src/com/company/List.java
@@ -1,9 +1,21 @@
 package com.company;
 
+/*
+
+List -- класс, представляющий из себя список пар (key, value),
+где key и value типа String
+
+ */
+
 public class List {
 
     List() {}
 
+
+    /*
+    Node --  класс, представляющий из себя элемент списка,
+    содержащий пару (key, value) и ссылку на следующий элемент
+     */
     private class Node {
 
         private Node next = null;
@@ -15,30 +27,61 @@ public class List {
             this.value = value;
         }
 
+
+        /*
+        метод getNext() возвращает следующий в списке элемент.
+         */
         public Node getNext() {
             return next;
         }
 
+
+        /*
+        метод setNext(next) делает элемент next следующим
+        в списке для данного элесента.
+         */
         public void setNext(Node next) {
             this.next = next;
         }
 
+
+        /*
+        метод getKey() возвращает значение key в данном
+        элементе списка.
+         */
         public String getKey() {
             return key;
         }
 
+        /*
+        метод getValue() возвращает значение value в данном
+        элементе списка
+         */
         public String getValue() {
             return value;
         }
 
+
+        /*
+        метод setValue(value) обновляет value для данного
+        элемента списка.
+         */
         public void setValue(String value) {
             this.value = value;
         }
 
     }
 
+    /*
+    head -- первый фиктивный элемент списка
+     */
     private Node head = new Node();
 
+
+    /*
+    метод findPrev(key) возвращает ссылку на элемент, предшествующий
+    в списке элементу с ключом key, или null, если такого элемента нет
+     */
     private Node findPrev(String key) {
         Node cur = head;
         while (cur.getNext() != null) {
@@ -50,6 +93,13 @@ public class List {
         return null;
     }
 
+
+    /*
+    метод insert(key, value) кладет в список пару (key, value) и,
+    если пара с ключом key уже была, то возвращает значение,
+    которое было ключу key раньше, и удаляет эту пару,
+    а если такой пары раньше не было, то возвращает null.
+     */
     public String insert(String key, String value) {
         Node prev = findPrev(key);
         if (prev == null) {
@@ -64,6 +114,11 @@ public class List {
         }
     }
 
+
+    /*
+    метод erase(key) удаляет пару с ключом key и возвращает значение
+    value из этой пары или null, если такой пары не было.
+     */
     public String erase(String key) {
         Node prev = findPrev(key);
         if (prev == null) {
@@ -75,10 +130,19 @@ public class List {
         return str;
     }
 
+
+    /*
+    метод empty() возвращает true, если список пуст и false иначе.
+     */
     public boolean empty() {
         return head.getNext() == null;
     }
 
+
+    /*
+    метод get(key) возвращает значение value, соответствующее key,
+    или null, если пары (key, value) нет в списке.
+     */
     public String get(String key) {
         Node prev = findPrev(key);
         if (prev == null) {
@@ -88,6 +152,10 @@ public class List {
         return prev.getNext().getValue();
     }
 
+
+    /*
+    метод clear() очищает список.
+     */
     public void clear() {
         head = new Node();
     }
