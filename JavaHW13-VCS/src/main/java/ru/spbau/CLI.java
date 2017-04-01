01220694edf57b63de4afa5e8741fe87ce33fdaa
tbelova <yukikomodo@gmail.com>
2017-04-01 13:58:25 +0300

    Add new class to parse arguments.

---
+++ JavaHW13-VCS/src/main/java/ru/spbau/CLI.java
@@ -0,0 +1,236 @@
+package ru.spbau;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CLI {
+
+    public static boolean tryInit(String[] args, Path path) {
+        if (!args[0].equals("init")) {
+            return false;
+        }
+        if (args.length > 1) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+        try {
+            Repository.initRepository(path);
+        } catch (IOException | MyExceptions.IsNotDirectoryException e) {
+            e.printStackTrace();
+        } catch (MyExceptions.AlreadyExistsException e) {
+            System.out.println("Repository already exists.");
+        }
+
+        return true;
+    }
+
+    public static boolean tryRemove(String[] args, Path path) {
+        if (!args[0].equals("remove")) {
+            return false;
+        }
+        if (args.length > 1) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+        try {
+            Repository.removeRepository(path);
+        } catch (IOException | MyExceptions.NotFoundException | MyExceptions.IsNotDirectoryException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public static Repository getRepository(Path path) {
+        Repository repository;
+        try {
+            repository = Repository.getRepository(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (MyExceptions.NotFoundException e) {
+            System.out.println("Can't load repository.");
+            return null;
+        }
+        return repository;
+    }
+
+    public static boolean tryBranch(String[] args, Repository repository) {
+        if (!args[0].equals("branch")) {
+            return false;
+        }
+        if (args.length == 1) {
+            try {
+                System.out.println(repository.getCurrentBranch());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (MyExceptions.WrongFormatException e) {
+                System.out.println("Something went wrong.");
+            }
+            return true;
+        }
+        if (args.length > 2) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+        try {
+            repository.branch(args[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MyExceptions.WrongFormatException e) {
+            System.out.println("Something went wrong.");
+        } catch (MyExceptions.AlreadyExistsException e) {
+            System.out.println("This branch already exists.");
+        }
+
+        return true;
+    }
+
+    public static boolean tryCheckout(String[] args, Repository repository) {
+        if (!args[0].equals("checkout")) {
+            return false;
+        }
+        if (args.length == 1) {
+            System.out.println("Too few arguments.");
+            return true;
+        }
+        if (args.length > 2) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+
+        try {
+            repository.checkout(args[1]);
+        } catch (MyExceptions.NotFoundException e) {
+            System.out.println("No such branch or commit.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public static boolean tryDeleteBranch(String[] args, Repository repository) {
+        if (!args[0].equals("branch_rm")) {
+            return false;
+        }
+        if (args.length == 1) {
+            System.out.println("Too few arguments.");
+            return true;
+        }
+        if (args.length > 2) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+
+        try {
+            repository.removeBranch(args[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MyExceptions.WrongFormatException e) {
+            System.out.println("Something went wrong.");
+        }
+
+        return true;
+    }
+
+    public static boolean tryAdd(String[] args, Repository repository) {
+        if (!args[0].equals("add")) {
+            return false;
+        }
+        if (args.length == 1) {
+            System.out.println("Too few arguments.");
+            return true;
+        }
+        if (args.length > 2) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+
+        try {
+            repository.add(Paths.get(args[1]));
+        } catch (MyExceptions.WrongFormatException e) {
+            e.printStackTrace();
+            //System.out.println("Something went wrong.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+
+    public static boolean tryCommit(String[] args, Repository repository) {
+        if (!args[0].equals("commit")) {
+            return false;
+        }
+        if (args.length == 1) {
+            System.out.println("Too few arguments.");
+            return true;
+        }
+        if (args.length > 2) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+
+        try {
+            repository.commit(args[1]);
+        } catch (MyExceptions.WrongFormatException e) {
+            System.out.println("Something went wrong.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public static boolean tryLog(String[] args, Repository repository) {
+        if (!args[0].equals("log")) {
+            return false;
+        }
+        if (args.length > 1) {
+            System.out.println("Too many arguments.");
+            return true;
+        }
+
+        try {
+            List<CommitWithMessage> log = repository.log();
+            for (CommitWithMessage commitWithMessage: log) {
+                System.out.println(commitWithMessage.getCommit() + "\n" + commitWithMessage.getMessage() +
+                        commitWithMessage.getAuthor() + "\n" + commitWithMessage.getDate() + "\n");
+            }
+        } catch (MyExceptions.WrongFormatException e) {
+            System.out.println("Something went wrong.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public static boolean tryMerge(String[] args, Repository repository) {
+        if (!args[0].equals("merge")) {
+            return false;
+        }
+        if (args.length != 2) {
+            System.out.println("Wrong number of arguments.");
+            return true;
+        }
+
+        try {
+            repository.merge(args[1]);
+        } catch (MyExceptions.NotFoundException e) {
+            System.out.println("No such branch.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MyExceptions.WrongFormatException e) {
+            System.out.println("Something went wrong.");
+        }
+
+        return true;
+    }
+
+
+}
--- JavaHW13-VCS/src/main/java/ru/spbau/Main.java
+++ JavaHW13-VCS/src/main/java/ru/spbau/Main.java
@@ -1,9 +1,7 @@
 package ru.spbau;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
 
@@ -16,196 +14,24 @@ public class Main {
 
         Path currentPath = Paths.get(System.getProperty("user.dir"));
 
-        if (args[0].equals("init")) {
-            if (args.length > 1) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-            try {
-                Repository.initRepository(currentPath);
-            } catch (IOException | MyExceptions.IsNotDirectoryException e) {
-                e.printStackTrace();
-            } catch (MyExceptions.AlreadyExistsException e) {
-                System.out.println("Repository already exists.");
-            }
-            return;
+        boolean knownCommand = false;
+
+        knownCommand |= CLI.tryInit(args, currentPath);
+        knownCommand |= CLI.tryRemove(args, currentPath);
+
+        Repository repository = CLI.getRepository(currentPath);
+
+        knownCommand |= CLI.tryBranch(args, repository);
+        knownCommand |= CLI.tryCheckout(args, repository);
+        knownCommand |= CLI.tryDeleteBranch(args, repository);
+        knownCommand |= CLI.tryAdd(args, repository);
+        knownCommand |= CLI.tryCommit(args, repository);
+        knownCommand |= CLI.tryLog(args, repository);
+        knownCommand |= CLI.tryMerge(args, repository);
+
+        if (!knownCommand) {
+            System.out.println("Unknown command.");
         }
-
-        if (args[0].equals("remove")) {
-            if (args.length > 1) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-            try {
-                Repository.removeRepository(Paths.get(System.getProperty("user.dir")));
-            } catch (IOException | MyExceptions.NotFoundException | MyExceptions.IsNotDirectoryException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Repository repository;
-        try {
-            repository = Repository.getRepository(currentPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        } catch (MyExceptions.NotFoundException e) {
-            System.out.println("Can't load repository.");
-            return;
-        }
-
-        if (args[0].equals("branch")) {
-            if (args.length == 1) {
-                try {
-                    System.out.println(repository.getCurrentBranch());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (MyExceptions.WrongFormatException e) {
-                    System.out.println("Something went wrong.");
-                }
-                return;
-            }
-            if (args.length > 2) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-            try {
-                repository.branch(args[1]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (MyExceptions.WrongFormatException e) {
-                System.out.println("Something went wrong.");
-            } catch (MyExceptions.AlreadyExistsException e) {
-                System.out.println("This branch already exists.");
-            }
-
-            return;
-        }
-
-        if (args[0].equals("checkout")) {
-            if (args.length == 1) {
-                System.out.println("Too few arguments.");
-                return;
-            }
-            if (args.length > 2) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-
-            try {
-                repository.checkout(args[1]);
-            } catch (MyExceptions.NotFoundException e) {
-                System.out.println("No such branch or commit.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return;
-        }
-
-        if (args[0].equals("branch_rm")) {
-            if (args.length == 1) {
-                System.out.println("Too few arguments.");
-                return;
-            }
-            if (args.length > 2) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-
-            try {
-                repository.removeBranch(args[1]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (MyExceptions.WrongFormatException e) {
-                System.out.println("Something went wrong.");
-            }
-
-            return;
-        }
-
-        if (args[0].equals("add")) {
-            if (args.length == 1) {
-                System.out.println("Too few arguments.");
-                return;
-            }
-            if (args.length > 2) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-
-            try {
-                repository.add(Paths.get(args[1]));
-            } catch (MyExceptions.WrongFormatException e) {
-                e.printStackTrace();
-                //System.out.println("Something went wrong.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return;
-        }
-
-        if (args[0].equals("commit")) {
-            if (args.length == 1) {
-                System.out.println("Too few arguments.");
-                return;
-            }
-            if (args.length > 2) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-
-            try {
-                repository.commit(args[1]);
-            } catch (MyExceptions.WrongFormatException e) {
-                System.out.println("Something went wrong.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return;
-        }
-
-        if (args[0].equals("log")) {
-            if (args.length > 1) {
-                System.out.println("Too many arguments.");
-                return;
-            }
-
-            try {
-                List<CommitWithMessage> log = repository.log();
-                for (CommitWithMessage commitWithMessage: log) {
-                    System.out.println(commitWithMessage.getCommit() + "\n" + commitWithMessage.getMessage() +
-                    commitWithMessage.getAuthor() + "\n" + commitWithMessage.getDate() + "\n");
-                }
-            } catch (MyExceptions.WrongFormatException e) {
-                System.out.println("Something went wrong.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return;
-        }
-
-        if (args[0].equals("merge")) {
-            if (args.length != 2) {
-                System.out.println("Wrong number of arguments.");
-                return;
-            }
-
-            try {
-                repository.merge(args[1]);
-            } catch (MyExceptions.NotFoundException e) {
-                System.out.println("No such branch.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (MyExceptions.WrongFormatException e) {
-                System.out.println("Something went wrong.");
-            }
-        }
-
-        System.out.println("Unknown command.");
 
     }
 
